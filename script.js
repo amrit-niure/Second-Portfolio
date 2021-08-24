85517c2d6bf58b9c828e39ab4e5a2cfad1d0bdb2
amrit-niure <niureamrit2@gmail.com>
2021-08-24 16:03:07 +0545

    pinkish header

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const links = document.querySelectorAll('nav li');
 hamburger.addEventListener('click',() =>{
     navlinks.classList.toggle("open");
 })
+
+
+
+var lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll",function(){
+    var scrollTop =window.pageYOffset || document.documentElement.scrollTop;
+
+    if(scrollTop > lastScrollTop){
+        navbar.style.top = "-10vh";
+    }
+    else{
+        navbar.style.top="0";
+    }
+    lastScrollTop = scrollTop;
+})
